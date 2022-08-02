4d982de98bf62828aeabe1c7fcf7079ca34b90d4
gusmero <andreagusmara@outlook.it>
2022-08-02 18:15:32 +0200

    tutorial : acceleratione and wrapping objects around the screnn

--- venv/space_rocks/game.py
+++ venv/space_rocks/game.py
@@ -37,12 +37,14 @@ class SpaceRocks:
             self.spaceship.rotate(clockwise=True)
         elif is_key_pressed[pygame.K_LEFT]:
             self.spaceship.rotate(clockwise=False)
+        if is_key_pressed[pygame.K_UP]:
+            self.spaceship.accelerate()
 
 
 
 
     def _process_game_logic(self):
-        self.spaceship.move()
+        self.spaceship.move(self.screen)
 
     def _draw(self):
         self.screen.blit(self.background, (0, 0))
--- venv/space_rocks/models.py
+++ venv/space_rocks/models.py
@@ -2,8 +2,9 @@ from pygame.math import Vector2
 from pygame.transform import rotozoom
 
 #for loading sprite
-from utils import load_sprite
+from utils import load_sprite, wrap_position
 UP = Vector2(0, -1)
+
 
 class GameObject:
     def __init__(self, position, sprite, velocity):
@@ -16,15 +17,22 @@ class GameObject:
         blit_position = self.position - Vector2(self.radius)
         surface.blit(self.sprite, blit_position)
 
-    def move(self):
-        self.position = self.position + self.velocity
+    def move(self, surface):
+        self.position = wrap_position(self.position + self.velocity, surface)
 
     def collides_with(self, other_obj):
         distance = self.position.distance_to(other_obj.position)
         return distance < self.radius + other_obj.radius
 
+
+
+
+
 class Spaceship(GameObject):
+
     MANEUVERABILITY = 3
+    ACCELERATION = 0.25
+
     def __init__(self, position):
     # Make a copy of the original UP vector
         self.direction = Vector2(UP)
@@ -41,3 +49,6 @@ class Spaceship(GameObject):
         rotated_surface_size = Vector2(rotated_surface.get_size())
         blit_position = self.position - rotated_surface_size * 0.5
         surface.blit(rotated_surface, blit_position)
+
+    def accelerate(self):
+        self.velocity += self.direction * self.ACCELERATION
