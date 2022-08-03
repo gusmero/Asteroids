1c8d920eb8394ee8956fb913d441026eab5e0d92
gusmero <andreagusmara@outlook.it>
2022-08-03 18:58:57 +0200

    set up: continuos integration fix pylint error

--- venv/space_rocks/__main__.py
+++ venv/space_rocks/__main__.py
@@ -1,5 +1,9 @@
 from game import SpaceRocks
 
+
+#  create a __main__.py file in your space_rocks folder. 
+# This file will take care of creating a new instance of 
+# your game and starting it by running main_loop()
 if __name__ == "__main__":
     space_rocks = SpaceRocks()
     space_rocks.main_loop()
